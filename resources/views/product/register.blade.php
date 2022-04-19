93fd519d97513e6b5418ddb9aaef32b3e8db61ae
brooktewabe <brooktewabe00@gmail.com>
2022-04-19 14:46:04 +0300

    form

--- resources/views/product/register.blade.php
+++ resources/views/product/register.blade.php
@@ -1,2 +1,35 @@
 
-Registration page
+<html>
+    <body>
+    <center>
+<h3>Registration page</h3>
+<form>
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td><input type="name" name="name" required="" id="name" ></td>
+    </tr>
+    <tr>
+            <td>Unit:</td>
+            <td><input type="number" name="unit" required="" id="unit"></td>
+    </tr>
+    <tr>
+            <td>Price:</td>
+            <td><input type="number" name="price" required="" id="price" ></td>
+    </tr>
+    <tr>
+            <td>Quantity:</td>
+            <td><input type="number" name="quantity" required="" id="quantity" ></td>
+    </tr>
+    <tr>
+		<td>&nbsp;</td>
+	<td><input type="reset" value="Reset">
+		<input type="submit" name="insert" value="Register"></td>
+	</tr>
+
+    </table>
+</form>
+</center>
+
+</body>
+</html>
